da78905d854950bf0777bd5efd760dedf3981934
lhathazy <Lacika12!!>
2018-08-10 08:59:59 +0200

    Add some incorrect formatting

--- src/main/java/com/demo/MyUglyFormattedClass.java
+++ src/main/java/com/demo/MyUglyFormattedClass.java
@@ -2,16 +2,19 @@ package com.demo;
 
 public class MyUglyFormattedClass {
 
-	private String anyField;
+private String anyField;
 	private String someOtherField;
 
-	public MyUglyFormattedClass() {
+   public MyUglyFormattedClass() {
 		super();
 	}
 
 	public void doSomething() {
-		// this line seems to be the only one correctly formatted
-		// This one has one tab and 4 spaces
+        // this line seems to be the only one correctly formatted
+	    // This one has one tab and 4 spaces
 	}
+
+
+
 
 }
