7dc98663a946c5ea0452ac46fbf2dba551cce4d2
lhathazy <Lacika12!!>
2018-08-10 08:58:41 +0200

    Initial commit

---
+++ src/main/java/com/demo/MyUglyFormattedClass.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class MyUglyFormattedClass {
+
+	private String anyField;
+	private String someOtherField;
+
+	public MyUglyFormattedClass() {
+		super();
+	}
+
+	public void doSomething() {
+		// this line seems to be the only one correctly formatted
+		// This one has one tab and 4 spaces
+	}
+
+}
